ea4d97fab4eb6fe24c6ef6e2aa8bd10bfdd6094d
jimmy <jimmy127328@gmail.com>
2026-02-17 22:04:55 +0800

    02.17_deploy gh page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/order_v2',
+  assetPrefix: '/order_v2/',
 };
 
 export default nextConfig;
